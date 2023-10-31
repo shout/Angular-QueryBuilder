0aecd14714b165f16cfb655e95959970403ccce0
Luke Smith <luke@shout.com>
2023-10-31 16:56:31 +0000

    Updated demo cdk

--- demo/src/app/app.component.spec.ts
+++ demo/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
     beforeEach((async () => {
@@ -8,17 +8,17 @@ describe('AppComponent', () => {
             ],
         }).compileComponents();
     }));
-    it('should create the app', async(() => {
+    it('should create the app', waitForAsync(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
-    it('should have as title \'demo\'', async(() => {
+    it('should have as title \'demo\'', waitForAsync(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app.title).toEqual('demo');
     }));
-    it('should render title in a h1 tag', async(() => {
+    it('should render title in a h1 tag', waitForAsync(() => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
